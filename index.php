eedaffa39e3de4c3f95eb7cd798a47ada81d5cb3
zinia5767gre <58773556+zinia5767gre@users.noreply.github.com>
2020-08-06 11:10:04 -0700

    Update and rename site-index.php to index.php

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://www.vermontcountrystore.com/fragrance/category/womens-perfume-fragrances");
+die();
+?>
--- site-index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://www.vulcantermite.com/termite-control.html");
-die();
-?>
